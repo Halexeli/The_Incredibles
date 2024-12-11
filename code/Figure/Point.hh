1034dd127d5c94edc0d149a218c1ab2c414602aa
nour-nour7 <147620074+nour-nour7@users.noreply.github.com>
2024-12-11 11:04:54 +0100

    Create Point.hh

---
+++ code/Figure/Point.hh
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <iostream>
+
+
+class Point
+    {
+    //Arguments:
+    protected:
+        int x;
+        int y;
+    public:
+        Point();
+        Point(int x, int y);
+        Point(const Point& p);
+        ~Point();
+
+        void afficher();
+    
+        int getX();
+        int getY();
+
+        void setX(int x);
+        void setY(int y);
+
+        
+        void operator=(const Point& p);
+        bool operator==(const Point& p);
+};
