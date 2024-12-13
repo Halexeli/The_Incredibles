39df559f85a77a578feb1a5c595fd1cfdabe2475
nour-nour7 <147620074+nour-nour7@users.noreply.github.com>
2024-12-13 14:59:07 +0100

    Add files via upload

---
+++ code/Figure/Ellipse.cc
@@ -0,0 +1,34 @@
+#include "Ellipse.hh"
+
+
+Ellipse_virtuelle::Ellipse_virtuelle():Figure(){
+    a=0;
+}
+
+Ellipse_virtuelle::Ellipse_virtuelle(const Ellipse_virtuelle &p):Figure(p){
+    
+    a=p.a;
+
+}
+
+Ellipse_virtuelle::Ellipse_virtuelle(const Point& p, const double _a):Figure(p){
+    a=_a;
+}
+
+
+Cercle::Cercle():Ellipse_virtuelle(){
+}
+Cercle::Cercle(const Cercle& c):Ellipse_virtuelle(c){
+
+}
+Cercle::Cercle(const Point& p,const double _a):Ellipse_virtuelle(p,_a){
+
+}
+
+
+void afficher(){
+
+    sf::CircleShape shape(a);
+    shape.setPosition(10,20);
+    window.draw(circle);
+}
