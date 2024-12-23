d80d82c4391fca3eb97408fef887abbc0a266e31
halexeli <alex.faucheu@gmail.com>
2024-12-23 10:27:55 +0100

    test, makefile a faire

--- code/Affichage/main.cpp
+++ code/Affichage/main.cpp
@@ -1,27 +1,27 @@
 #include <iostream>
+#include "Screen.hh"
+#include "Page.hh"
+#include "Bouton.hh"
 
 int main()
 {
-    
+    Screen screen(500,750);
+    Page page1;
+    page1+=new Bouton(100,100,50,20,0);
+    page1+=new Texte(200, 100, 12, 0xFF0000FF, "test");
     // run the program as long as the window is open
-    while (window.isOpen())
+    while (screen.isOpen())
     {
         // check all the window's events that were triggered since the last iteration of the loop
         sf::Event event;
-        while (window.pollEvent(event))
+        while (screen.pollEvent(event))
         {
             // "close requested" event: we close the window
             if (event.type == sf::Event::Closed)
-            window.close();
+            screen.close();
         }
-
-        // clear the window with black color
-        window.clear(sf::Color::Black);
-
-        b.Render(10, window);// give the function reference on window
-
-        // end the current frame
-        window.display();
+        page1.draw(screen);
+        screen.render();
     }
 
     return 0;
